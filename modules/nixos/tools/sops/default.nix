02b64d1ca1d76946a1ee995afabe3e29478eb900
Adam Zwakenberg <adam@adamzwakk.com>
2025-03-09 16:41:39 -0400

    cleaned out sops stuff cause still being referenced

--- modules/nixos/tools/sops/default.nix
+++ modules/nixos/tools/sops/default.nix
@@ -13,27 +13,27 @@ let
   cfg = config.${namespace}.tools.sops;
 in
 {
-  imports = [ inputs.sops-nix.nixosModules.sops ];
+  # imports = [ inputs.sops-nix.nixosModules.sops ];
 
-  options.${namespace}.tools.sops = with types; {
-    enable = mkBoolOpt false "Enable sops";
-  };
+  # options.${namespace}.tools.sops = with types; {
+  #   enable = mkBoolOpt false "Enable sops";
+  # };
 
-  config = mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [
-      age
-      sops
-      ssh-to-age
-    ];
+  # config = mkIf cfg.enable {
+  #   environment.systemPackages = with pkgs; [
+  #     age
+  #     sops
+  #     ssh-to-age
+  #   ];
 
-    sops = {
-      defaultSopsFile = lib.snowfall.fs.get-file "secrets/secrets.yaml";
-      defaultSopsFormat = "yaml";
+  #   sops = {
+  #     defaultSopsFile = lib.snowfall.fs.get-file "secrets/secrets.yaml";
+  #     defaultSopsFormat = "yaml";
       
-      age.keyFile = "/home/${config.lv426.config.user.name}/.config/sops/age/keys.txt";
-      age.generateKey = true;
+  #     age.keyFile = "/home/${config.lv426.config.user.name}/.config/sops/age/keys.txt";
+  #     age.generateKey = true;
 
-      secrets.hudson_syncthing_id = {};
-    };
-  };
+  #     secrets.hudson_syncthing_id = {};
+  #   };
+  # };
 }
