95e9f0602d4a6287f60f33a5d4ef59b00f0af660
adamzwakk <adam@adamzwakk.com>
2025-08-20 17:21:43 -0400

    added quake aliases/desktop entry

--- home-config/config/gaming/quake/default.nix
+++ home-config/config/gaming/quake/default.nix
@@ -7,7 +7,23 @@
   ...
 }:
 {
-  home.packages = with pkgs; [
-    ironwail
+  home = {
+    packages = with pkgs; [
+      ironwail
+    ];
+
+    shellAliases.quake = "${pkgs.ironwail}/bin/ironwail -basedir ${config.home.homeDirectory}/Games/Quake/";
+  };
+
+  xdg.desktopEntries.quake = {
+    name = "Quake";
+    comment = "Launch Quake (Ironwail)";
+    exec = "${pkgs.ironwail}/bin/ironwail -basedir ${config.home.homeDirectory}/Games/Quake/";
+    categories = [ "Game" ];
+    terminal = false;
+  };
+
+  wayland.windowManager.hyprland.settings.windowrule = lib.mkAfter [
+    "float,class:^(ironwail)$"
   ];
 }
