2ddb8022adc399f2a7fe6fe60379eed42e6662e1
adamzwakk <adam@adamzwakk.com>
2025-09-09 08:36:19 -0400

    ff to music user

--- home-config/users/music.nix
+++ home-config/users/music.nix
@@ -10,6 +10,7 @@
     ../config/desktop/hyprland.nix
     ../config/desktop/bars/waybar
 
+    ../config/apps/_browsers/firefox      # Need for spotify login
     ../config/apps/_tui/spotify_player
   ];
 }
