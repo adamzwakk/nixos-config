0a83a2b1641978af1cc0a4d0718cfc271df3f319
adamzwakk <adam@adamzwakk.com>
2025-12-08 22:36:22 -0500

    added ryubing and igir

--- modules/home-config/config/gaming/emulation/default.nix
+++ modules/home-config/config/gaming/emulation/default.nix
@@ -9,6 +9,8 @@
 {
   home.packages = with pkgs; [
     flips
+    ryubing       # Switch Emulator
+    igir          # ROM Manager
     (retroarch.withCores (cores: with cores; [ ## https://github.com/NixOS/nixpkgs/tree/master/pkgs/applications/emulators/libretro/cores
         parallel-n64
         snes9x
