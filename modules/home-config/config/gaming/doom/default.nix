1a4a9da5d6a4858954aecb0038cd7c4aeb377f8f
adamzwakk <adam@adamzwakk.com>
2026-01-09 12:18:45 -0500

    added doom windowrules

--- modules/home-config/config/gaming/doom/default.nix
+++ modules/home-config/config/gaming/doom/default.nix
@@ -11,4 +11,9 @@
     uzdoom
     qzdl
   ];
+
+  wayland.windowManager.hyprland.settings.windowrule = lib.mkAfter [
+    "float on,match:class ^(zdl)$"
+    "float on,match:class ^(uzdoom)$"
+  ];
 }
